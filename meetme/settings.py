739defe4dfee324d9a2899b291a402aad227ae2b
Satria Bagus <satriabagus@ruangguru.com>
2017-08-31 19:36:55 +0700

    launch

--- meetme/settings.py
+++ meetme/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
