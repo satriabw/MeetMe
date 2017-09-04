5684ef78b8edd0eb0037875ce604025f55217c63
Satria Bagus <satria_bagus@hotmail.com>
2017-09-04 19:56:36 +0700

    change body respon for user-interest

--- account/utils.py
+++ account/utils.py
@@ -1,10 +1,12 @@
 
 import datetime
-
+import requests
+import json
+from urllib.request import Request, urlopen  # Python 3
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_jwt.settings import api_settings
 from django.contrib.auth import login, authenticate
 from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
@@ -71,6 +73,38 @@ class AuthLogin(APIView):
             return username
         except User.DoesNotExist:
             return None
+
+
+class PostToUserInterest(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, Format=None):
+        data = request.data
+        user_id = data.get("user", None)
+        interests = data.get('interest', None).split(",")
+        token = data.get('token', None)
+        response = {}
+        # change the url later
+        url = 'http://127.0.0.1:8000/api/v1/account/user-interest/'
+        try:
+            for interest in interests:
+                values = {
+                    'user' : user_id,
+                    'interest' : interest
+                }
+                headers = {"content-type": "application/json", 'Authorization' : 'jwt '+token}
+                req = requests.post(url, data=json.dumps(values), headers=headers)
+                key = 'interest_'+interest
+                response[key] = req.text
+
+            return Response({
+                'data' : response
+            },status=status.HTTP_200_OK)
+        except Exception:
+            return Response({
+                'data' : "error"
+            },status=status.HTTP_400_BAD_REQUEST)
+
 
 # class AuthLogin(APIView):
 #     permission_classes = (AllowAny,)
