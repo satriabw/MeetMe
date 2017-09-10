59ea43ad0a4e5bb3a6b59eabdd6950a3d31f8ab8
Satria Bagus <satria_bagus@hotmail.com>
2017-09-10 11:21:06 +0700

    add django cron

--- meetme/settings.py
+++ meetme/settings.py
@@ -186,5 +186,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # DATABASES['default'] =  dj_database_url.config()
 # DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 CRONJOBS = [
-    ('*/15 * * * *', 'myapp.cron.my_scheduled_job')
+    ('*/15 * * * *', 'meetme.cron.my_scheduled_job')
 ]
